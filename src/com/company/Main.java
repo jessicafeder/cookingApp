bd0debb14bdfce3516a53f47956a9c879b596af3
Jessica Feder <jessica.feder@student.kyh.se>
2020-10-13 14:56:21 +0200

    lagt till filer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
+import java.net.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 
 public class Main {
 
+    public static void recipeUrl() {
+        /*URL[] url = new URL[10];
+        URL url = new URL( "http://baeldung.com");
+        url[0] = ("http://baeldung.com");*/
+
+    }
+
+    public static void userInput(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name one protein: ");
+        String protein = sc.nextLine();  // Read user input
+        System.out.println("Your protein is: " + protein);  // Output user input
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        userInput();
+        String[] url = new String[10];
+        //System.out.println("https://stackoverflow.com/questions/15290223/declare-array-of-urls");
+        System.out.println("Hello World");
     }
 }
