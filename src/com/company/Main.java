d629b67c7581897bea6b45f9414cf0e99f0e1382
Jessica Feder <jessica.feder@student.kyh.se>
2020-10-15 10:37:43 +0200

    lagt till några rader i userInput

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,15 +15,19 @@ public class Main {
 
     public static void userInput(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Name one protein: ");
+        System.out.println("Name a protein you have: ");
         String protein = sc.nextLine();  // Read user input
-        System.out.println("Your protein is: " + protein);  // Output user input
+        System.out.println("Your chosen protein is: " + protein + "\n");  // Output user input
+        System.out.println("Name a carb you have: ");
+        String carb = sc.nextLine();
+        System.out.println("Your chosen carb is: " + carb);
+
     }
+
 
     public static void main(String[] args) {
         userInput();
-        String[] url = new String[10];
+        //String[] url = new String[10];
         //System.out.println("https://stackoverflow.com/questions/15290223/declare-array-of-urls");
-        System.out.println("Hello World");
     }
 }
