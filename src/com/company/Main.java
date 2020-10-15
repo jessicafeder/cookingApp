19be853e3883a8f8efad96bb46069cb80baafefd
Jessica Feder <jessica.feder@student.kyh.se>
2020-10-15 13:16:36 +0200

    Ändringar i Read File kod

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,6 @@
 package com.company;
+
+import java.io.*;
 import java.net.*;
 import java.sql.SQLOutput;
 import java.util.Scanner;
@@ -6,14 +8,31 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void recipeUrl() {
-        /*URL[] url = new URL[10];
-        URL url = new URL( "http://baeldung.com");
-        url[0] = ("http://baeldung.com");*/
+    public static void main(String[] args) {
+        userInput();
+        readFile();
 
+
+        //String[] url = new String[10];
+        //System.out.println("https://stackoverflow.com/questions/15290223/declare-array-of-urls");
     }
+public static void readFile(){
+    try {
+        File recipe = new File("/Users/jessi/Documents/cookingApp/src/Files");
+        Scanner myRecipe = new Scanner(recipe);
+        while (myRecipe.hasNextLine()) {
+            String input = myRecipe.nextLine();
+            System.out.println(input);
+        }
+        myRecipe.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("Not found");
+        e.printStackTrace();
+    }
+}
 
-    public static void userInput(){
+    public static void userInput() {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Name a protein you have: ");
         String protein = sc.nextLine();  // Read user input
@@ -21,13 +40,9 @@ public class Main {
         System.out.println("Name a carb you have: ");
         String carb = sc.nextLine();
         System.out.println("Your chosen carb is: " + carb);
+        if (protein.equals("Chicken") && carb.equals("Rice")) {
+            System.out.println("Here is your recipe");
+        }
 
-    }
-
-
-    public static void main(String[] args) {
-        userInput();
-        //String[] url = new String[10];
-        //System.out.println("https://stackoverflow.com/questions/15290223/declare-array-of-urls");
     }
 }
