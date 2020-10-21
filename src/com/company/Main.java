1e3698a43c785dde7ecc1237cb7fb61a4054fc79
Jessica Feder <jessica.feder@student.kyh.se>
2020-10-21 16:13:29 +0200

    Tog bort kommenteringsblock

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -61,23 +61,4 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-
-
-    /*public static void playSound() {
-        File ramsay = new File("sounds/ramsaysoundwav.wav");
-        try {
-            Clip clip = AudioSystem.getClip();
-            clip.open(AudioSystem.getAudioInputStream(ramsay));
-            clip.start();
-
-            Thread.sleep(clip.getMicrosecondLength()/1000);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-    }*/
 }
