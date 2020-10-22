7304d397b5983958329819e1968c57b33cb70333
Jessica Feder <jessica.feder@student.kyh.se>
2020-10-22 15:07:41 +0200

    no message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import javax.sound.sampled.*;
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -17,22 +17,21 @@ public class Main {
 
         System.out.println("\nFor a list of recipes press 1, to choose your own ingredients press 2: ");
         String inputChoice = sc.nextLine();
-        if(inputChoice.equals("1")){
+        if (inputChoice.equals("1")) {
             System.out.println("Great! Here is a list of recipes for you to choose from: \n ");
             FileToArray.fileToArray();
             System.out.println("\nDid you fancy any of the recipes? If yes type Y, if not write your own by typing N: ");
             String inputChoice1 = sc.nextLine();
-            if (inputChoice1.equals("Y")){
+            if (inputChoice1.equals("Y")) {
                 System.out.println("\nGood luck and have fun! \uD83C\uDF74\n------------------------------------");
-            }
-            else if(inputChoice1.equals("N")){
+            } else if (inputChoice1.equals("N")) {
                 System.out.println("Write your recipe in the following order -> PROTEIN WITH CARB: URL ");
                 WriteRecipe.writeRecipe();
                 System.out.println("\nGood luck and have fun! \uD83C\uDF74\n------------------------------------");
             }
             return;
 
-        } else if(inputChoice.equals("2")){
+        } else if (inputChoice.equals("2")) {
             System.out.println("Fun! Name one of the following proteins: Chicken, Salmon, Beef or Pork ");
         }
         String protein = sc.nextLine();
@@ -40,7 +39,7 @@ public class Main {
 
         System.out.println("Name one of the following carbs: Rice, Pasta, Potato ");
         String carb = sc.nextLine();
-        System.out.println("Your chosen ingredients are: " + protein + " and " + carb +   "\n");
+        System.out.println("Your chosen ingredients are: " + protein + " and " + carb + "\n");
 
         myFile.readFile(protein, carb);
         System.out.println("\nGood luck and have fun! \uD83C\uDF74\n------------------------------------");
