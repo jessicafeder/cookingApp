2e6fec2a1ca80508c7877104288fde879badab54
Andreas Berg <andreas.berg2@student.kyh.se>
2020-10-26 14:26:04 +0100

    Skapade test för ReadFile!

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -1,9 +1,26 @@
 package com.company;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
+    @Test
+    @DisplayName("Check if input equals a string in a file")
+    void readFileTest() {
+        ReadFile readFile = new ReadFile();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        readFile.readFile("Chicken","Rice");
+        assertEquals("CHICKEN CURRY WITH RICE: https://www.ica.se/recept/kycklingcurry-med-ris-727027/", outContent.toString().trim());
+    }
+
+
 
 
 }
